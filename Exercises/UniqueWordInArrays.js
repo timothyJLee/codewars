71f5093dca603d2569c3664e2aa82d1288593747
Timothy Lee <timothy.j.lee.cs@gmail.com>
2023-12-27 11:31:22 -0500

    finishing

--- Exercises/UniqueWordInArrays.js
+++ Exercises/UniqueWordInArrays.js
@@ -1,16 +1,22 @@
 /*
-   ******IN PROGRESS******
 Given an array of string arrays, find all the words that only appear in a single array
 ex: [['mshike','mko','mshike','bozho'],['mshike','bamapi']] 
         returns ['mko','bozho','bamapi']
-*/
 
+        Solution:
+        - Clean individual arrays of multiples
+        - Create counting hashmap to keep track of multiples vs unique in constant time
+        - convert to array and filter for counts of 1
+        - convert to object and print
+*/
 
 const array = [
   ["mshike", "mko", "mshike", "bozho"],
   ["mshike", "bamapi"],
 ];
 
+// cleaning individual arrays of multiple values because we dont care about those
+// converting to Obj
 const cleanedObj = (() => {     // arrow IIFE
   let cleaned = [];
   array.forEach((wordArray) => {
@@ -19,39 +25,22 @@ const cleanedObj = (() => {     // arrow IIFE
   return cleaned;
 })();
 
-//console.log(cleanedObj);
- //singleArrayWords(array);
- singleArrayWordsHashmap(cleanedObj);
+console.log(cleanedObj);
+ singleArrayWordsCountingHashmap(cleanedObj);
 
-function singleArrayWords(wordSetsObj) {
-  // single word array
-  let singleWords = [];
-  //for loop for first array
-  for (let i = 0; i < array.length; i++) {
-    //for loop second array
-    for (let j = 0; j < array[i].length; j++) {
-      if (array) console.log(array[i][j]);
-    }
-    console.log(array[i]);
-  }
-}
-
-function singleArrayWordsBuiltIn(wordSetsObj) {
-  // single word array
-  let singleWords = [];
-
-  //for loop for first array
-  for (let i = 0; i < array.length; i++) {
-    //built in string compare method
-  }
-}
-
-function singleArrayWordsHashmap(wordSetsObj) {
+function singleArrayWordsCountingHashmap(wordSetsObj) {
   let hashmap = {};
   wordSetsObj.forEach((wordArray) => {
     wordArray.forEach((word) => {
       // counting hashmap object
-      console.log(word);
+      // ternary operator to set to 1 if place in hm is undefined else increment
+      hashmap[word] =(hashmap[word] == undefined) ? 1 : hashmap[word] + 1;
     });
   });
+
+  // convert to array for easy filtering
+  const uniqueWords = Object.entries(hashmap).filter(([key, value]) => value == 1);
+  // convert back to obj and log
+  const uniqueWordsObject = Object.fromEntries(uniqueWords);
+  console.log(uniqueWordsObject);
 }
