3d5fcd5a4ed4136502676274e20389cdfba1f28a
Timothy Lee <timothy.j.lee.cs@gmail.com>
2023-12-29 15:06:31 -0500

    merge sorted linkedlists

---
+++ grind75/MergeTwoSortedLists.js
@@ -0,0 +1,70 @@
+/*
+21. Merge Two Sorted Lists
+Easy
+20.7K
+2K
+Companies
+You are given the heads of two sorted linked lists list1 and list2.
+
+Merge the two lists into one sorted list. The list should be made by splicing together the nodes of the first two lists.
+
+Return the head of the merged linked list.
+
+ 
+
+Example 1:
+
+
+Input: list1 = [1,2,4], list2 = [1,3,4]
+Output: [1,1,2,3,4,4]
+Example 2:
+
+Input: list1 = [], list2 = []
+Output: []
+Example 3:
+
+Input: list1 = [], list2 = [0]
+Output: [0]
+ 
+
+Constraints:
+
+The number of nodes in both lists is in the range [0, 50].
+-100 <= Node.val <= 100
+Both list1 and list2 are sorted in non-decreasing order.
+*/
+
+ // Definition for singly-linked list.
+  function ListNode(val, next) {
+      this.val = (val===undefined ? 0 : val)
+      this.next = (next===undefined ? null : next)
+  }
+
+lists1 = [[1,2,4],[],[]];
+lists2 = [[1,3,4],[],[0]];
+
+function mergeTwoLists(list1, list2) {
+    let mergedList = new ListNode();
+    let mergedListHead = mergedList;
+
+    while(list1 && list2){
+        if(list1.val < list2.val){
+            mergedList.next = list1;
+            list1 = list1.next;
+        } else {
+            mergedList.next = list2;
+            list2 = list2.next;
+        }
+        mergedList = mergedList.next;
+    }
+
+    if(list1){
+        mergedList.next = list1;
+    }
+    if(list2){
+        mergedList.next = list2;
+    }
+
+    mergedListHead = mergedListHead.next;
+    return mergedListHead;
+}
