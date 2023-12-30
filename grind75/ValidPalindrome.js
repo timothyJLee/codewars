03dc8503f422d7f0f371837f4ef843e9554d3f7e
Timothy Lee <timothy.j.lee.cs@gmail.com>
2023-12-30 10:48:16 -0500

    regex

--- grind75/ValidPalindrome.js
+++ grind75/ValidPalindrome.js
@@ -1,9 +1,5 @@
 /*
 125. Valid Palindrome
-Easy
-8.7K
-8.1K
-Companies
 A phrase is a palindrome if, after converting all uppercase letters into lowercase letters and removing all non-alphanumeric characters, it reads the same forward and backward. Alphanumeric characters include letters and numbers.
 
 Given a string s, return true if it is a palindrome, or false otherwise.
@@ -37,14 +33,14 @@ s consists only of printable ASCII characters.
 function isPalindrome(s) {
   const stringProccessed = (s) => {
     let processedString = s
-      .replace(/[^A-Za-z]/g, "") //regex replace non word with nothing
+      .replace(/[^A-Za-z0-9]/g, "") //regex replace non alphanumeric with nothing
       .toLowerCase() //all letters to lowercase
       .replaceAll(" ", ""); //replace all spaces with nothing
     return processedString; //return string
   };
   const string = stringProccessed(s);
   for (let i = 0; i < string.length / 2; i++) {
-    if (!(string[i] == string[(string.length - 1) - i])) {
+    if (!(string[i] === string[(string.length - 1) - i])) {
       return false;
     }
   }
